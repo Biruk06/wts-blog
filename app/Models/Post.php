ebc88a87110e2670cec871296991c664774cd60d
Biruk <sergei20000620@gmail.com>
2026-06-02 18:49:54 +0300

    Добавил поддержку мягкого удаления (Soft Delete)
    - Создана миграция для добавления поля deleted_at в таблицу posts.
    - В модель Post подключен трейт SoftDeletes.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,15 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    // Разрешаем массовое заполнение этих полей при создании поста
+    use HasFactory, SoftDeletes;
+    
     protected $fillable = ['user_id', 'title', 'text'];
 
-    // Указываем обратную связь: пост принадлежит пользователю (автору)
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
